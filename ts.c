6ad041c36027ebdb155a97a023f81f6621ae7600
WillMerges <wdmerges@gmail.com>
2021-02-16 21:40:51 -0500

    added example

--- ts.c
+++ ts.c
@@ -1,4 +1,4 @@
-#include "tiny_sched.h"
+#include "ts.h"
 #include <string.h>
 
 // this is in bss and should be initialized to zero
@@ -9,15 +9,15 @@ tiny_task_t tasks[TS_MAX_TASKS];
 unsigned char ttid_map[TS_MAX_TASKS];
 
 
-int ts_add(tiny_task_t task) {
+int ts_add(tiny_task_t* task) {
     for(int i = 0; i < TS_MAX_TASKS; i++) {
         if(ttid_map[i] == 0) {
             ttid_map[i] = 1;
-            tasks[i] = task;
+            tasks[i] = *task;
             return i;
         }
     }
-    return -1; // no space
+    return TS_ERR_TTID; // no space
 }
 
 void ts_rem(int ttid) {
@@ -31,15 +31,24 @@ void ts_rem(int ttid) {
 }
 
 // not all that great of time complexity,  ¯\_(ツ)_/¯
-void schedule(int* stop_var, int stop_cond) {
+// always run a higher priority task regardless how far behind a lower one is
+void ts_schedule(int* stop_var, int stop_cond) {
+    int dummy = 1;
+    if(stop_var == NULL) {
+        stop_var = &dummy;
+        stop_cond = 0; // just loop forever
+    }
+
     long int curr_time;
     long int best_ttid = -1;
     while(*stop_var != stop_cond) {
         curr_time = ts_systime();
         for(size_t i = 0; i < TS_MAX_TASKS; i++) {
             if(ttid_map[i] == 1) {
-                if(tasks[i].start_time > curr_time) {
-                    if(tasks[i].start_time < tasks[best_ttid].start_time + TS_PEEK_LENGTH) {
+                if(tasks[i].start_time < curr_time) {
+                    if(best_ttid == -1) {
+                        best_ttid = i;
+                    } else if(tasks[i].start_time < tasks[best_ttid].start_time) {
                         if(tasks[i].priority > tasks[best_ttid].priority) {
                             best_ttid = i;
                         }
@@ -48,8 +57,9 @@ void schedule(int* stop_var, int stop_cond) {
             }
         }
         if(best_ttid != -1) {
-            best_ttid = -1;
+            tasks[best_ttid].start_time = curr_time - 1;
             tasks[best_ttid].task(&tasks[best_ttid]);
+            best_ttid = -1;
         }
     }
 }
--- ts.h
+++ ts.h
@@ -15,36 +15,33 @@
 // the ttid returned when an error occurs (no more room to store task)
 #define TS_ERR_TTID -1
 
-// how far to peek into the future to look for a higher priority task to run
-#define TS_PEEK_LENGTH 10
-
 // this needs to be defined by the system!
 // the extern is unnecessary, just a reminder to define it!
 extern long int ts_systime();
 
 // priority level
 typedef enum {
-    LOW, MEDIUM, HIGH
+    LOW_PRIORITY, MEDIUM_PRIORITY, HIGH_PRIORITY
 } priority_t;
 
 // task structure
 typedef struct tiny_task tiny_task_t;
 struct tiny_task {
     int ttid; // tiny task id, set by the scheduler
-    long int start_time; // requested start time, set by user
+    long int start_time; // requested start time, set by user, units are what ts_systime uses
     priority_t priority; // priority level, set by user
     void (*task) (tiny_task_t*); // task function, defined by programmer
 };
 
 // add a task to the scheduler
 // returns ttid
-int ts_add(tiny_task_t task);
+int ts_add(tiny_task_t* task);
 
 // remove a task from the scheduler
 void ts_rem(int ttid);
 
 // runs the scheduler
 // will not exit unless *stop_var == stop_cond
-void schedule(int* stop_var, int stop_cond);
+void ts_schedule(int* stop_var, int stop_cond);
 
 #endif
